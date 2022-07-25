8f18abac4174dcc1f1bd83f842dfa6460b1a9920
Lucas Souza <lucasbmbr@hotmail.com>
2022-07-24 22:06:07 -0300

    segundo commit

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -8,7 +8,79 @@ export class Item {
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  update(){
+    this.sellIn -= 1
+    let decay = 1
+    if (this.sellIn < 0) {
+      decay = decay * 2
+    }
+    let newQuality = this.quality - decay
+    if (newQuality >= 0) {
+      this.quality = newQuality
+    } else {
+      this.quality = 0
+    }
+  }
 }
+
+
+export class Lendario extends Item {
+
+  constructor (nome) {
+    super(nome, 0, 80)
+  }
+  update() {
+  }
+
+}
+
+export class Entrada extends Item {
+  update() {
+    this.sellIn -= 1
+    let appreciation = 1
+    if (this.sellIn <= 5) {
+      appreciation = 3
+    } else if (this.sellIn <= 10){
+      appreciation = 2
+    }
+    if ((this.quality + appreciation) < 50) {
+      this.quality += appreciation
+    } else {
+      this.quality = 50
+    }
+    if (this.sellIn < 0) {
+      this.quality = 0
+    }
+  }
+}
+
+export class Valorizavel extends Item {
+  update() {
+    this.sellIn -= 1
+    if (this.quality < 50) {
+      this.quality += 1
+    }
+  }
+}
+
+export class Conjurado extends Item {
+  update() {
+    this.sellIn -= 1
+    let decay = 2
+    if (this.sellIn < 0) {
+      decay = decay * 2
+    }
+    let newQuality = this.quality - decay
+    if (newQuality >= 0) {
+      this.quality = newQuality
+    } else {
+      this.quality = 0
+    }
+  }
+}
+
+
 
 export class GildedRose {
   items: Array<Item>;
@@ -18,52 +90,29 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
-    }
-
-    return this.items;
+    this.items.forEach(Item => Item.update())
+    return this.items
   }
 }
+
+function main() {
+  let gr = new GildedRose()
+
+  gr.items.push(new Item("+5 Dexterity Vest", 10, 20))
+  gr.items.push(new Item("Elixir of the Mongoose", 5, 7))
+  gr.items.push(new Valorizavel("Aged Brie", 2, 0))
+  gr.items.push(new Entrada("Backstage passes to a TAFKAL80ETC concert", 15, 20))
+  gr.items.push(new Lendario("Sulfuras, Hand of Ragnaros"))
+  gr.items.push(new Conjurado("Conjured Mana Cake", 3, 6))
+
+
+  for (let index = 0; index < 5; index++) {
+    gr.updateQuality()
+  }
+
+  for (let item of gr.items) {
+    console.log(item)
+  }
+}
+
+main()
